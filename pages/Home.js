5da15a6cb253944b10bbf1723a8629dc0df06c17
XRichardroidX <oguzieibehrichard@gmail.com>
2023-01-25 08:22:18 +0100

    Add pages

---
+++ pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Book from '../components/Book';
+import Form from '../components/Form';
+
+const Home = () => (
+  <>
+    <Book title=" Oliver Twist" author="Charles Dickens" />
+    <Form />
+  </>
+);
+
+export default Home;
