60b70858784612048819615692fb91fdb9fcc1d6
XRichardroidX <oguzieibehrichard@gmail.com>
2023-01-25 16:04:25 +0100

    Refactore code base

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const Form = () => (
-  <div>
-    <form>
-      <input type="text" name="title" id="title" placeholder="Add Book Title" />
-      <select name="author" id="author">
-        <option value="shakespear">Shakespear</option>
-        <option value="arthur-c-d">Bill Nye</option>
-        <option value="jk-rowling">Michio Kaku</option>
-        <option value="jane-austin">Dr. Jordan Peterson</option>
-      </select>
-      <button type="button">Add Book</button>
-    </form>
-  </div>
-);
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newBook = {
+      id: v4(),
+      title,
+      author,
+    };
+
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+    // setGenre('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Book's Title"
+          id="title"
+          name="title"
+          onChange={(event) => setTitle(event.target.value)}
+          value={title}
+        />
+        <input
+          type="text"
+          placeholder="Author's Name"
+          name="author"
+          id="author"
+          onChange={(event) => setAuthor(event.target.value)}
+          value={author}
+        />
+        <button type="submit">Add Book</button>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
---
+++ src/redux/books/books.js
@@ -0,0 +1,44 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { v4 } from 'uuid';
+
+const ADD = 'bookstore/books/ADD';
+const REMOVE = 'bookstore/books/REMOVE';
+
+// Actions
+const addBook = (book) => ({
+  type: ADD,
+  book,
+});
+
+const removeBook = (id) => ({
+  type: REMOVE,
+  id,
+});
+
+// Reducer
+const bookReducer = (state = [
+  {
+    id: v4(), title: 'Oliver Twist', author: 'Charles Dikens', genre: 'sc-fi',
+  },
+  {
+    id: v4(), title: 'The World as Will and Representation', author: 'Arthur Schopenhauer', genre: 'philosophy',
+  },
+  {
+    id: v4(), title: 'Thus Spoke Zarathustra', author: 'Friedrich Nietzsche', genre: 'philosophy',
+  },
+  {
+    id: v4(), title: 'Les Misérables', author: 'Victor Hugo', genre: 'historical fiction',
+  },
+], action = {}) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.book];
+    case REMOVE:
+      return [...state.filter((book) => book.id !== action.id)];
+    default:
+      return state;
+  }
+};
+
+export { addBook, removeBook };
+export default bookReducer;
